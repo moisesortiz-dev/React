f78587991c609ad4b7eb0d9c852b834f2dbe7e29
Moisés Ortiz Carmona <135473469+moisesortiz-dev@users.noreply.github.com>
2024-04-13 14:28:06 +0200

    Add files via upload

---
+++ 06 - Actividad6/Actividad2.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Actividad2( {reyes} ) {
+
+    const eliminar = (e) =>{
+        e.target.parentNode.remove();
+    }
+
+  return (
+    <>
+      {reyes.filter(e=> !e.nombre.includes("g")).
+      map(e=><div key={e.nombre}>{e.nombre}<button onClick={eliminar}>Borrar</button></div>)
+      }
+    </>
+  )
+}
+
+export default Actividad2
